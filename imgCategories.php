e47a11ec9a1e749164d49539c215e1b7fda29ea6
Doug <karaballo.douglas@hotmail.com>
2022-03-22 07:53:28 -0400

    Primer commit

---
+++ imgCategories.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Img Categories
+    Plugin URI:
+    Description: Plugin con la finalidad de poder agregar imagenes a las categorias del wordpress
+    Version: 0.1
+    Author: Douglas Caraballo
+    Author URI: https://github.com/Douglas-Caraballo
+    License:
+*/
