5fb3af09fcb0fa450583a349c224add7d8b2ade9
Doug <karaballo.douglas@hotmail.com>
2022-03-24 08:06:41 -0400

    Cambios a la funcion jQuery para mostrar la imagen al momento de registrar una categoria

--- admin/controler/AddImg.php
+++ admin/controler/AddImg.php
@@ -1,7 +1,10 @@
 <?php
+if(!class_exists('AddImg')){
+
+
 class AddImg{
     // Add a form field in the category page
-    public function add_category_img($taxonomy){?>
+    public function add_category_img(){?>
     <div>
         <label for="category-image-id"><?php _e('Image'); ?></label>
         <input type="hidden" id="category-image-id" name="category-image-id" class="custom_media_url" value="">
@@ -16,14 +19,14 @@ class AddImg{
 
     // save the form field
     public function save_category_img($term_id, $tt_id){
-        if(isset($_POST['category-image-id'])&& '' !==$_POST['category-image-id']){
-            $image = $_POST['category-image-id'];
-            add_term_meta( $term_id,'category-image-id',$image, true);
+        if( isset( $_POST['category-image-id'] ) && '' !== $_POST['category-image-id'] ){
+            $image = $_POST['categoría-imagen-id'];
+            add_term_meta( $term_id, 'category-image-id', $image, true);
         }
     }
 
     // edit the from field
-    public function update_category_img($term, $taxonomy){?>
+    public function update_category_img($term){?>
         <tr class="form-field term-group-wrap">
             <th scope="row">
                 <label for="category-image-id"><?php _e('Image'); ?></label>
@@ -61,22 +64,22 @@ class AddImg{
     // add sript
     public function add_scrip(){?>
         <script>
-            JQuery(document).ready(function($){
+            jQuery(document).ready(function($){
                 function media_upload(button_class){
                     var _custom_media = true,
                         _orig_send_attachment = wp.media.editor.send.attachment;
-                    $('body').on('click', button_class, function(e){
+                    $('body').on('click', button_class, function(e) {
                         var button_id = '#'+$(this).attr('id');
                         var send_attachment_bkp = wp.media.editor.send.attachment;
                         var button = $(button_id);
                         _custom_media = true;
-                        wp.media.editor.send.attachment = function (props, attachment){
+                        wp.media.editor.send.attachment = function(props, attachment){
                             if(_custom_media){
                                 $('#category-image-id').val(attachment.id);
-                                $('#category-image-wrapper').html('<img class="custom_media_image" src"" style="margin:0;padding:0; max-height:100px;float:none;"/>');
-                                $('#category-image-wrapper .custom_media_image').attr('src', attachment.url).css('display','block');
+                                $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+                                $('#category-image-wrapper .custom_media_image').attr('src',attachment.url).css('display','block');
                             }else{
-                                return _orig_send_attachment.apply(button_id,[props, attachment]);
+                                return _orig_send_attachment.apply(button_id, [props, attachment]);
                             }
                         }
                         wp.media.editor.open(button);
@@ -86,13 +89,13 @@ class AddImg{
                 media_upload('.tax_media_button.button');
                 $('body').on('click','.tax_media_remove',function(){
                     $('#category-image-id').val('');
-                    $('#category-image-wrapper').html('<img class="custom_media_image" src"" style="margin:0;padding:0; max-height:100px;float:none;"/>');
+                    $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
                 });
-                $(document).ajaxComplete(function(event, xhr, settings){
-                    var queryStringArr=settings.data.split('&');
-                    if($.inArray('action=add-tag',queryStringArr)!== -1){
-                        var xml= xhr.responseXML;
-                        $response=$(xml).find('term_id').text();
+                $(document).ajaxComplete(function(event, xhr, settings) {
+                    var queryStringArr = settings.data.split('&');
+                    if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+                        var xml = xhr.responseXML;
+                        $response = $(xml).find('term_id').text();
                         if($response!=""){
                             $('#category-image-wrapper').html('');
                         }
@@ -102,4 +105,5 @@ class AddImg{
         </script>
     <?php
     }
+}
 }
