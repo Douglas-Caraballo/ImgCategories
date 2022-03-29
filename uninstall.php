d4077960aa80f76c5736fc07b8b0913c7b62ff55
Doug <karaballo.douglas@hotmail.com>
2022-03-29 13:18:56 -0400

    Creacion de un nuevo uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,12 @@
 <?php
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+global $wpdb;
+$tableName = $wpdb->prefix."termmeta";
+$metaKey = "category-image-id";
+
+$query = "DELETE * FROM $tableName WHERE meta_key= $metaKey";
+
+$wpdb -> query($query);
