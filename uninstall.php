1edb44284ad84effaf2e744bddd928dd9c9b567b
Doug <karaballo.douglas@hotmail.com>
2022-04-05 08:42:51 -0400

    Cambios al desinstalador del plugin

--- uninstall.php
+++ uninstall.php
@@ -3,10 +3,17 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-/*global $wpdb;
+global $wpdb;
 $tableName = $wpdb->prefix."termmeta";
 $metaKey = "category-image-id";
 
+$termMetaRecords = $wpdb-> get_results(
+    "SELECT term_id FROM $tableName WHERE meta_key = $metaKey",ARRAY_A
+);
+foreach($termMetaRecords as $termMeta){
+    delete_term_meta( $termMeta->term_id, $metaKey);
+}
+/*
 $wpdb->query("DELETE FROM $tableName WHERE meta_key = $metaKey");
 
 $query = "DELETE * FROM $tableName WHERE meta_key= $metaKey";
